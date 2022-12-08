accd8722413ab75f965c7adcabd007d53aef9133
Senchenko Amina <sch.aa.vk@gmail.com>
2022-12-08 20:42:39 +0600

    feat: change news.js file and add changes to INewsAPI

---
+++ migration-newip-to-ts/src/components/view/news/news.ts
@@ -0,0 +1,39 @@
+import './news.css';
+import { INewsAPI } from "../../../types/index";
+
+export class News {
+    draw(data: INewsAPI[]) {
+        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+
+        const fragment = document.createDocumentFragment();
+        const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
+
+        news.forEach((item , idx) => {
+            const newsClone = newsItemTemp.content.cloneNode(true) as Element;
+
+            if (idx % 2) (newsClone.querySelector('.news__item') as Element).classList.add('alt');
+
+            (newsClone.querySelector('.news__meta-photo') as HTMLElement).style.backgroundImage = `url(${
+                item.urlToImage || 'img/news_placeholder.jpg'
+            })`;
+            (newsClone.querySelector('.news__meta-author') as Element).textContent = item.author || item.source.name;
+            (newsClone.querySelector('.news__meta-date') as Element).textContent = item.publishedAt
+                .slice(0, 10)
+                .split('-')
+                .reverse()
+                .join('-');
+
+            (newsClone.querySelector('.news__description-title') as Element).textContent = item.title;
+            (newsClone.querySelector('.news__description-source') as Element).textContent = item.source.name;
+            (newsClone.querySelector('.news__description-content') as Element).textContent = item.description;
+            (newsClone.querySelector('.news__read-more a') as Element).setAttribute('href', item.url);
+
+            fragment.append(newsClone);
+        });
+
+        (document.querySelector('.news') as Element).innerHTML = '';
+        (document.querySelector('.news') as Element).appendChild(fragment);
+    }
+}
+
+export default News;
--- migration-newip-to-ts/src/types/index.ts
+++ migration-newip-to-ts/src/types/index.ts
@@ -1,12 +1,38 @@
-interface INewsAPI {
-  status: string,
-  sources: [
-    id: string,
-    name: string,
-    description: string,
-    url: string,
-    category: string,
-    language: string,
-    country: string,
-  ],
+export interface INewsAPI {
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+  status: string;
+  totalResults: number;
+  source: {
+    id: string;
+    name: string;
+    description: string;
+    url: string;
+    category: string;
+    language: string;
+    country: string;
+  };
+  articles: {
+    source: {
+      id: string;
+      name: string;
+      description: string;
+      url: string;
+      category: string;
+      language: string;
+      country: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+  },
 }
