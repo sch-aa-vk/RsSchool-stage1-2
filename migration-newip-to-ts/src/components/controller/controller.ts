a92cf27f4defdb85dd7b975819cc951f0ee0f45e
Senchenko Amina <sch.aa.vk@gmail.com>
2022-12-09 23:09:33 +0600

    feat: change controller.js file

--- migration-newip-to-ts/src/components/controller/controller.ts
+++ migration-newip-to-ts/src/components/controller/controller.ts
@@ -1,7 +1,7 @@
-import AppLoader from './appLoader';
+import { AppLoader } from './appLoader';
 
-class AppController extends AppLoader {
-    getSources(callback) {
+export class AppController extends AppLoader {
+    getSources(callback: () => void) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,13 +10,13 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: () => void) {
+        let target = e.target as Element;
+        const newsContainer = e.currentTarget as Element;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId = target.getAttribute('data-source-id') as string;
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +31,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as Element;
         }
     }
 }
