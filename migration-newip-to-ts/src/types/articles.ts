ea0dd902013bd349917c4a82cd1161289dbbe8a8
Senchenko Amina <sch.aa.vk@gmail.com>
2022-12-11 18:44:40 +0600

    fix: split INewsApi into two other interfaces

---
+++ migration-newip-to-ts/src/types/articles.ts
@@ -0,0 +1,14 @@
+export interface IArticles {
+  id: string,
+  name: string,
+  category: string,
+  language: string,
+  country: string,
+  author: string,
+  title: string,
+  description: string,
+  url: string,
+  urlToImage: string,
+  publishedAt: string,
+  content: string,
+}
--- migration-newip-to-ts/src/types/index.ts
+++ migration-newip-to-ts/src/types/index.ts
@@ -1,36 +1,9 @@
+import { IArticles } from "./articles";
+import { ISources } from "./sources";
+
 export interface INewsAPI {
   status: string;
   totalResults: number;
-  sources: [{
-    id: string,
-    name: string,
-    description: string,
-    url: string,
-    category: string,
-    language: string,
-    country: string,
-  }];
-  articles: [{
-    sources: {
-      id: string,
-      name: string,
-      description: string,
-      url: string,
-      category: string,
-      language: string,
-      country: string,
-    };
-    id: string,
-    name: string,
-    category: string,
-    language: string,
-    country: string,
-    author: string,
-    title: string,
-    description: string,
-    url: string,
-    urlToImage: string,
-    publishedAt: string,
-    content: string,
-  }],
+  articles: [IArticles];
+  sources: [ISources];
 }
