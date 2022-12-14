9dea5ed3fb00fa076116da855e1adcf5c9118f26
Senchenko Amina <sch.aa.vk@gmail.com>
2022-12-14 17:03:30 +0600

    fix: change url

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import { Loader } from './loader';
 
 export class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: '2abfeab2a9b24960bed230ca2f8aa89a',
         });
     }
