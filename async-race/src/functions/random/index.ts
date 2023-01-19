0fcdcfbe67eb3e0a17cac9fc74b5073790917c99
sch-aa-vk <sch.aa.vk@gmail.com>
2023-01-19 20:24:27 +0600

    feat: add generate 100 cars function

---
+++ async-race/src/functions/random/index.ts
@@ -0,0 +1,3 @@
+export function random(n: number) {
+  return Math.ceil(Math.random() * n);
+}
--- async-race/src/layouts/form/index.ts
+++ async-race/src/layouts/form/index.ts
@@ -1,6 +1,10 @@
 import { Button } from '../../components/button/index';
 import { Container } from '../../components/container/index';
 import { InputContainer } from '../../components/inputContainer/index';
+import { machines } from '../../database/car';
+import { colors } from '../../database/color';
+import { createCar } from '../../functions/createCar/index';
+import { random } from '../../functions/random/index';
 import './style.css';
 
 export const Form = () => {
@@ -12,7 +16,12 @@ export const Form = () => {
 
   const buttonRace = Button('race', () => {});
   const buttonReset = Button('reset', () => {});
-  const buttonGenCars = Button('generate cars', () => {});
+  const buttonGenCars = Button('generate cars', (e) => {
+    e?.preventDefault();
+    for (let i = 0; i < 100; i++) {
+     createCar('http://127.0.0.1:3000/garage', {name: machines[random(machines.length - 1)], color: colors[random(colors.length)]});
+    }
+  });
 
   const block = Container([buttonRace, buttonReset, buttonGenCars], 'row wrap');
 
