c4da5bdb3a031b081efdfaed2f08b6058f56851f
sch-aa-vk <sch.aa.vk@gmail.com>
2023-01-30 23:34:54 +0600

    feat: add presentation

--- migration-newip-to-ts/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-import { IArticles } from "./articles";
-import { ISources } from "./sources";
-
-export interface INewsAPI {
-  status: string;
-  totalResults: number;
-  articles: [IArticles];
-  sources: [ISources];
-}
